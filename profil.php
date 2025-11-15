1e7be15dfe95bcaa158f6f444db0728eda0d89d2
anuchii <anaflorea1701@icloud.com>
2025-11-15 17:57:14 +0100

    Profil-Seite Haupt-Header hinzugefügt.

--- profil.php
+++ profil.php
@@ -47,20 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['profileImage'])) {
             <div class="col-12 col-lg-10 p-0">
 
                 <!-- Abmelden-Bar -->
-                <nav class="navbar navbar-expand-lg bg-secondary border-bottom px-0 me-0">
-                    <div class="container-fluid px-0">
-                        <ul class="navbar-nav ms-auto me-0">
-                            <li class="nav-item me-0">
-
-                                <a class="btn btn-warning text-dark pe-3 me-3" href="login.php">
-                                    Abmelden <i class="bi bi-arrow-bar-right  text-dark ps-1"></i>
-                                </a>
-
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-
+               <?php include 'header.php';?>
                 <!-- Header -->
                 <header class="py-4 border-bottom p-3">
                     <h2>Mein Profil</h2>
