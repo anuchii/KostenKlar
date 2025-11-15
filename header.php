8072e0567489c9413ef61d6322f4e5dbafd6fea6
anuchii <anaflorea1701@icloud.com>
2025-11-15 17:55:55 +0100

    🗣️  header.php Datei hinzugefügt.

---
+++ header.php
@@ -0,0 +1,13 @@
+ <header > <!--Geschmackssache: class="sticky-top"-->
+ <nav class="navbar navbar-expand-lg bg-secondary border-bottom px-0 me-0">
+     <div class="container-fluid px-0">
+         <ul class="navbar-nav ms-auto me-0">
+             <li class="nav-item me-0">
+                 <a class="btn btn-warning text-dark pe-3 me-3" href="login.php">
+                     Abmelden <i class="bi bi-arrow-bar-right text-dark ps-1"></i>
+                 </a>
+             </li>
+         </ul>
+     </div>
+ </nav>
+ </header>
