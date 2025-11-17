035c769514738ca95eb3391a6b10d7289a7d1f9e
anuchii <anaflorea1701@icloud.com>
2025-11-17 10:47:25 +0100

    Dashboard: vorläufige Anpassungen – Rücksprache nötig

--- dashboard.php
+++ dashboard.php
@@ -1,76 +1,100 @@
 <!DOCTYPE html>
-<html lang="en">
+<!--weil die Seite auf Deutsch ist für den User, das hilft dem Browser für zb. die Autokorrektur-->
+<html lang="de">
 
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Dashboard</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 
 <body>
 	<div class="container-fluid">
-		<div class="row" style="height: 100vh">
+
+		<!--row" style="height: 100vh-->
+		<div class="row min-vh-100">
 
 			<?php include 'sidebar.php'; ?>
 
-			<div class="col-10">
-				<nav class="navbar navbar-expand-lg bg-secondary border-bottom">
-					<div class="container-fluid">
-						<ul class="navbar-nav ms-auto">
-							<li class="nav-item">
-								<a class="nav-link text-white" href="login.php">Abmelden<i class="bi bi-arrow-bar-right text-white px-1"></i></a>
-							</li>
-						</ul>
-					</div>
-				</nav>
+			<div class="col-12 col-lg-10 p-0">
+				<?php include 'header.php'; ?>
 
-				<header class="py-4">
+				<header class="py-4 border-bottom p-3">
 					<h2>Willkommen, Benutzer!</h2>
 				</header>
 
-				<section class="py-4">
+				<section class="py-4 p-3">
 					<h4>Buchungsübersicht</h4>
 				</section>
-
-				<div class="mb-3">
-					<label for="month" class="col-form-label">Abrechnungsmonat</label>
-					<input type="month" class="form-control" id="month" name="month" style="width: auto" ;>
+				<!--Monat-Jahr Auswahl-->
+				<div class="row g-2">
+					<div class="col-auto p-3">
+						<select class="form-select" id="month" name="month" style="width: auto;">
+							<option value="">Monat auswählen …</option>
+							<option value="01">Januar </option>
+							<option value="02">Februar </option>
+							<option value="03">März </option>
+							<option value="04">April </option>
+							<option value="05">Mai </option>
+							<option value="06">Juni </option>
+							<option value="07">Juli </option>
+							<option value="08">August </option>
+							<option value="09">September </option>
+							<option value="10">Oktober </option>
+							<option value="11">November </option>
+							<option value="12">Dezember </option>
+						</select>
+					</div>
+					<div class="col-auto p-3">
+						<select class="form-select" id="year" name="year">
+							<option value="">Jahr wählen</option>
+							<?php
+							$currentYear = date("Y");
+							for ($i = $currentYear; $i >= $currentYear - 3; $i--) {
+								echo "<option value='$i'>$i</option>";
+							}
+							?>
+						</select>
+					</div>
 				</div>
 
-				<table class="table">
-					<thead>
-						<tr>
-							<th scope="col">Datum</th>
-							<th scope="col">Bezeichnung</th>
-							<th scope="col">Betrag</th>
-							<th scope="col">Kategorie</th>
-							<th scope="col">Notiz</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>30.10.2025</td>
-							<td>Gehalt</td>
-							<td class="text-success">1545,00</td>
-							<td>Gehalt</td>
-							<td>Oktober 2025</td>
-						</tr>
-						<tr>
-							<td>30.10.2025</td>
-							<td>Einkauf</td>
-							<td class="text-danger">-22,54</td>
-							<td>Lebensmittel</td>
-							<td>Billa</td>
-						</tr>
-					</tbody>
-				</table>
+				<!--Tabelle: Buchungsübersicht-->
+				<div class="mb-3 p-3 mx-auto">
+					<table class="table table-hover ">
+						<thead class="table-light">
+							<tr>
+								<th scope="col">Datum</th>
+								<th scope="col">Bezeichnung</th>
+								<th scope="col">Betrag</th>
+								<th scope="col">Kategorie</th>
+								<th scope="col">Notiz</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>30.10.2025</td>
+								<td>Gehalt</td>
+								<td class="text-success fw-semibold">1545,00</td>
+								<td>Gehalt</td>
+								<td>Oktober 2025</td>
+							</tr>
+							<tr>
+								<td>30.10.2025</td>
+								<td>Einkauf</td>
+								<td class="text-danger fw-semibold">-22,54</td>
+								<td>Lebensmittel</td>
+								<td>Billa</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 
 				<!-- Button trigger modal -->
-				<div class="mb-3">
+				<div class="mb-3 p-3">
 					<button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#new-modal">
-						<i class="bi bi-plus-circle text-white"></i>
+						<i class="bi bi-plus-circle text-black"></i>
 					</button>
 				</div>
 
@@ -136,25 +160,24 @@
 						</div>
 					</div>
 				</div>
-
-				<section class="py-4">
+				<!--Zusammenfassung: Gesamntübersicht-->
+				<section class="py-3 p-3">
 					<h4>Gesamtübersicht</h4>
 				</section>
-
 				<div class="d-sm-flex flex-sm-row gap-3">
-					<div class="my-3">
-						<div class="card" style="width: 18rem;">
+					<div class="my-3 p-3">
+						<div class="card pd-3" style="width: 18rem;">
 							<div class="card-body">
-								<h6 class="card-subtitle mb-2 text-body-secondary text-success">Einnahmen</h5>
+								<h6 class="card-subtitle mb-2 text-body-secondary text-success fw-semibold">Einnahmen</h5>
 									<h5 class="card-title text-success">EUR 1545,00
 								</h6>
 							</div>
 						</div>
 					</div>
-					<div class="my-3">
-						<div class="card" style="width: 18rem;">
+					<div class="my-3 p-3">
+						<div class="card " style="width: 18rem;">
 							<div class="card-body">
-								<h6 class="card-subtitle mb-2 text-body-secondary text-danger">Ausgaben</h5>
+								<h6 class="card-subtitle mb-2 text-body-secondary text-danger fw-semibold">Ausgaben</h5>
 									<h5 class="card-title text-danger">EUR 22,54
 								</h6>
 							</div>
@@ -162,9 +185,7 @@
 					</div>
 				</div>
 			</div>
-				
 		</div>
-	
 	</div>
 	<?php include 'footer.php'; ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
