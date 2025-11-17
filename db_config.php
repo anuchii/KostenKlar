23d6ff9c4b222a5d19731c574358e7c440cc1507
anuchii <anaflorea1701@icloud.com>
2025-11-17 17:01:22 +0100

    db-dump + db_config und users.php wurde eingefügt.

---
+++ db_config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$dbhost = "localhost";
+$port = 8889;
+$dbname = "KostenKlar";
+$dbuser = "root";
+$dbpassword = "root";
+
+try {
+    $pdo = new PDO("mysql:host={$dbhost};port={$port};dbname={$dbname}", $dbuser, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+} catch (PDOException $e) {
+    echo "Database connection failed: {$e->getMessage()}";
+    die();
+}
+return $pdo;
---
+++ users.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once __DIR__ . '/db_config.php';
+
+
+function createUser($userData, $pdo)
+{
+    // Hash password
+    $userData["password"] = password_hash($userData["password"], PASSWORD_DEFAULT);
+
+    // Prepare SQL statement
+    $statement = $pdo->prepare(
+        "INSERT INTO users (first_name, last_name, email, password, role, status)
+        VALUES (:first_name, :last_name, :email, :password, 'user', 'active')"
+    );
+
+    // Bind values
+    $statement->bindValue(":first_name", $userData["first_name"]);
+    $statement->bindValue(":last_name", $userData["last_name"]);
+    $statement->bindValue(":email", $userData["email"]);
+    $statement->bindValue(":password", $userData["password"]);
+
+    // Execute statement
+    $success = $statement->execute();
+
+    return $success;
+}
+
+function getUserIDByEmail($email, $pdo)
+{
+    // Prepare SQL statement
+    $statement = $pdo->prepare(
+        "SELECT user_id FROM users WHERE email = :email"
+    );
+
+    // Bind values
+    $statement->bindValue(":email", $email);
+    $statement->execute();
+
+    // Fetch database entries
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
+function getPasswordByUserID($user_id, $pdo)
+{
+    // Prepare SQL statement
+    $statement = $pdo->prepare(
+        "SELECT password FROM users WHERE user_id = :user_id"
+    );
+
+    // Bind values
+    $statement->bindValue(":user_id", $user_id, PDO::PARAM_INT);
+    $statement->execute();
+
+    // Fetch database entries
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
+function getPasswordByEmail($email, $pdo)
+{
+    // Prepare SQL statement
+    $statement = $pdo->prepare(
+        "SELECT password FROM users WHERE email = :email"
+    );
+
+    // Bind values
+    $statement->bindValue(":email", $email);
+    $statement->execute();
+
+    // Fetch database entries
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $result;
+}
+
+// Checks if email address is already registered
+function isEmailRegistered($email, $pdo)
+{
+    $user_id = getUserIDByEmail($email, $pdo);
+    $count = count($user_id);
+
+    if ($count === 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function getUserDataByUserID($user_id, $pdo)
+{
+    // Prepare SQL statement
+    $statement = $pdo->prepare(
+        "SELECT * FROM users WHERE user_id = :user_id"
+    );
+
+    // Bind values
+    $statement->bindValue(":user_id", $user_id, PDO::PARAM_INT);
+    $statement->execute();
+
+    // Fetch database entries
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $result;
+}
