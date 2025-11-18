58f92774d612b0121c7d5dca83c62889be365266
Markus Pieber <markus.pieber@gmx.at>
2025-11-18 23:14:02 +0100

    Datenbankkonfiguration angepasst (zweite Version auskommentiert)

--- db_config.php
+++ db_config.php
@@ -1,11 +1,13 @@
 <?php
 
-
 $dbhost = "localhost";
-$port = 8889;
-$dbname = "KostenKlar";
-$dbuser = "root";
-$dbpassword = "root";
+$port = 3306;
+// $port = 8889;
+$dbname = "kostenklar";
+// $dbname = "Kostenklar";
+$dbuser = "";
+// $dbpassword = "root";
+
 
 try {
     $pdo = new PDO("mysql:host={$dbhost};port={$port};dbname={$dbname}", $dbuser, $dbpassword);
