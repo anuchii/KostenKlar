e7ec07f7ceb0b569d5acbe9b0206e2265ff873ca
anuchii <anaflorea1701@icloud.com>
2026-01-19 10:52:41 +0100

    fix(navbar): remove broken toggler button from auth navbar

--- app/includes/nav_auth.php
+++ app/includes/nav_auth.php
@@ -4,10 +4,5 @@
                 <img class="me-2" src="<?= asset_url('images/logo_schnell3.png') ?>" width="50" alt="logo_kostenklar">
                 KostenKlar
             </a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         </div>
     </nav>
